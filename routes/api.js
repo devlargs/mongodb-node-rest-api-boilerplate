ea9093e9253f993c5ecac3caa8ed066ae7676966
Ralph Largo <devlargs@gmail.com>
2018-05-21 19:31:09 +0800

    read authorization header

--- routes/api.js
+++ routes/api.js
@@ -44,12 +44,15 @@ router.post('/authenticate', function (req, res, next) {
 });
 
 router.post('/verifyToken', function (req, res, next) {
-    try {
-        var verify = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MjY2MjI4MDYsInVzZXIiOnsiaWQiOiJmdWNraW5nIHBpZWNlIG9mIHNoaXQifSwiYXBwbGljYXRpb25JZCI6ImJhbGFuYXIiLCJpYXQiOjE1MjY2MTkyMDZ9.KZopXPwHQU-Fl9Wl-yV9jUnK-WMqfa4Bz0wV4Ax4VpY', 'secret');
-        res.send({ status: 200, verify, message: 'Successfully authenticated.' });
-    } catch (ex) {
-        res.send({ status: 401, message: ex.message })
-    }
+    res.send({
+        a: req.headers.authorization
+    })
+    // try {
+    //     var verify = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MjY2MjI4MDYsInVzZXIiOnsiaWQiOiJmdWNraW5nIHBpZWNlIG9mIHNoaXQifSwiYXBwbGljYXRpb25JZCI6ImJhbGFuYXIiLCJpYXQiOjE1MjY2MTkyMDZ9.KZopXPwHQU-Fl9Wl-yV9jUnK-WMqfa4Bz0wV4Ax4VpY', 'secret');
+    //     res.send({ status: 200, verify, message: 'Successfully authenticated.' });
+    // } catch (ex) {
+    //     res.send({ status: 401, message: ex.message })
+    // }
 });
 
 router.get('/getEntity/:table', function (req, res, next) {
