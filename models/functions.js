2daaa8f94d8310d61a08667f0e445e1690396984
Ralph Largo <devlargs@gmail.com>
2018-05-22 19:52:50 +0800

    added additional logic upon encrytion

--- models/functions.js
+++ models/functions.js
@@ -5,13 +5,13 @@ var algorithm = 'aes-256-ctr';
 exports.encrypt = (text) => {
     var cipher = crypto.createCipher(algorithm, encryptionPassword)
     var c = cipher.update(text, 'utf8', 'hex')
-    c += cipher.final('hex');
-    return c;
+    c += cipher.final('hex')
+    return c.split('').reverse().join('')
 }
 
 exports.decrypt = (hash) => {
     var decipher = crypto.createDecipher(algorithm, encryptionPassword)
-    var d = decipher.update(hash, 'hex', 'utf8')
-    d += decipher.final('utf8');
-    return d;
+    var d = decipher.update(hash.split('').reverse().join(''), 'hex', 'utf8')
+    d += decipher.final('utf8')
+    return d
 }
