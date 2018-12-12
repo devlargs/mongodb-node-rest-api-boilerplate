eb9fd2425dec3d16c58f23ebd2a102568da5d426
Ralph Largo <devlargs@gmail.com>
2018-12-13 03:56:24 +0800

    Set Database environment variables

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-//To make things secure, use environment variables
+
+const { MONGODB_PASSWORD, MONGODB_USERNAME } = process.env;
 
 exports.database = {
-    url: 'mongodb://localhost:27017',
-    name: 'test',
-    password: 'admin',
-    username: 'admin'
+    url: `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0-shard-00-00-fprc1.mongodb.net:27017,cluster0-shard-00-01-fprc1.mongodb.net:27017,cluster0-shard-00-02-fprc1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`,
+    name: 'campusfiles',
+    password: MONGODB_PASSWORD,
+    username: MONGODB_USERNAME
 };
 exports.encryptionPassword = 'm34j8fhcltu@djchqnsju924uj';
 exports.secretKey = 'secret';
