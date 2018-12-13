6da225955107c1160111563d6410649253273dcc
Ralph Largo <devlargs@gmail.com>
2018-12-13 15:55:10 +0800

    Replace some obsolete to es6 codes

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
-var { getCollections } = require('../models/database');
+const { Router } = require("express");
+const router = Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var api = require('../models/api');
-var { decrypt, encrypt } = require('../models/functions');
-var { secretKey } = require('../config');
+const { getCollections } = require("../models/database");
+const api = require("../models/api");
+const { decrypt, encrypt } = require("../models/functions");
+const { secretKey } = require("../config");
 
-var generateToken = (payload) => {
-    let data = JSON.stringify({
+const generateToken = (payload) => {
+    const data = JSON.stringify({
         dateCreated: new Date().toString(),
         exp: Math.floor(Date.now()),
         ...payload
@@ -17,7 +17,7 @@ var generateToken = (payload) => {
     return jwt.sign(encrypt(data), secretKey);
 }
 
-router.post('/authenticate', function (req, res) {
+router.post("/authenticate", (req, res) => {
     /**
      * @api {POST} /authenticate Get Token
      * @apiGroup Authentication
@@ -28,44 +28,44 @@ router.post('/authenticate', function (req, res) {
      * @apiSuccess {String} token Used for Authorization header on request
     */
 
-    if (!req.body.email) {
-        res.send({ message: 'email is not defined', status: 403 })
-    } else if (!req.body.password) {
-        res.send({ message: 'password is not defined', status: 403 })
+    const { email, password } = req.body;
+
+    if (!email) {
+        res.send({ message: "Email is not defined", status: 403 })
+    } else if (!password) {
+        res.send({ message: "Password is not defined", status: 403 })
     } else {
         api.Get({
-            table: 'users',
-            filter: {
-                email: req.body.email
-            }
-        }).then(function (response) {
+            table: "users",
+            filter: { email }
+        }).then((response) => {
             if (response.lists.length) {
-                bcrypt.compare(req.body.password, response.lists[0].password, function (err, correct) {
+                bcrypt.compare(password, response.lists[0].password, (_, correct) => {
                     if (correct) {
                         res.send({
                             token: generateToken({ userId: response.lists[0]._id }),
-                            message: 'Successfully authenticated.',
+                            message: "Successfully authenticated.",
                             status: 200
                         })
                     } else {
-                        res.send({ message: 'Incorrect password.', status: 403 });
+                        res.send({ message: "Incorrect password.", status: 403 });
                     }
                 });
             } else {
-                res.send({ message: 'Username does not exist.', status: 404 });
+                res.send({ message: "Username does not exist.", status: 404 });
             }
         })
     }
 });
 
-router.use(function (req, res, next) {
-    var token = req.headers.authorization || req.query.token;
+router.use((req, res, next) => {
+    const token = req.headers.authorization || req.query.token;
     if (token) {
-        jwt.verify(token, secretKey, function (err, decoded) {
+        jwt.verify(token, secretKey, (err, decoded) => {
             if (err) {
                 res.send({
                     status: 412,
-                    message: 'Failed to authenticate token.'
+                    message: "Failed to authenticate token."
                 });
             } else {
                 getCollections(coll => {
@@ -79,12 +79,12 @@ router.use(function (req, res, next) {
     } else {
         res.send({
             status: 403,
-            message: 'No token provided.'
+            message: "No token provided."
         });
     }
 });
 
-router.get('/:table', async function (req, res) {
+router.get("/:table", async (req, res) => {
     const collections = await new Promise((resolve) => {
         getCollections((res) => {
             resolve(res);
@@ -103,19 +103,19 @@ router.get('/:table', async function (req, res) {
             });
         } else {
             res.send({
-                message: 'Table not found.',
+                message: "Table not found.",
                 status: 404
             })
         }
     } else {
         res.send({
-            message: 'Please enter table.',
+            message: "Please enter table.",
             status: 404
         })
     }
 });
 
-router.get('/:table/:id', async function (req, res, next) {
+router.get("/:table/:id", async (req, res) => {
     /**
      * @api {GET} /:table/:id Get Entity
      * @apiGroup Entities
@@ -123,8 +123,8 @@ router.get('/:table/:id', async function (req, res, next) {
      * @apiParam (Query) {String} table Specify database table/collection
      * @apiParam (Query) {String} [id] Pass it on /getEntity/:table/:id if you want to return specific data
      *
-     * @apiParam (Body) {Object} [filter] Pass specific attributes to return filtered queries. e.g. { address: 'Makati'}
-     * @apiParam (Body) {Object} [fields] Pass this to return specific attributes that you only need. e.g. ['Name', 'Address'] returns [{Name: '', Address: ''}, ...{ and_so_on }] only
+     * @apiParam (Body) {Object} [filter] Pass specific attributes to return filtered queries. e.g. { address: "Makati"}
+     * @apiParam (Body) {Object} [fields] Pass this to return specific attributes that you only need. e.g. ["Name", "Address"] returns [{Name: "", Address: ""}, ...{ and_so_on }] only
      * 
      * @apiHeader {String} Authorization Insert generated token here to validate request.
      * 
@@ -152,19 +152,19 @@ router.get('/:table/:id', async function (req, res, next) {
             });
         } else {
             res.send({
-                message: 'Table not found.',
+                message: "Table not found.",
                 status: 404
             })
         }
     } else {
         res.send({
-            message: 'Please enter table.',
+            message: "Please enter table.",
             status: 404
         })
     }
 });
 
-router.post('/:table', function (req, res) {
+router.post("/:table", (req, res) => {
     /**
      * @api {POST} /:table Post Entity
      * @apiGroup Entities
@@ -192,11 +192,11 @@ router.post('/:table', function (req, res) {
     })
 });
 
-router.put('/:table', (_, res) => {
+router.put("/:table", (_, res) => {
     res.send({ status: 400, message: "Please enter id" })
 });
 
-router.put('/:table/:id', function (req, res) {
+router.put("/:table/:id", (req, res) => {
     /**
      * @api {PUT} /:table/:id Put Entity
      * @apiGroup Entities
@@ -216,17 +216,17 @@ router.put('/:table/:id', function (req, res) {
     api.Put({
         ...req.params,
         newData: req.body.formData
-    }).then(function (response) {
+    }).then((response) => {
         res.send({
             ...response,
             newToken: generateToken({ userId: req.userId })
         })
-    }).catch(function (ex) {
+    }).catch((ex) => {
         res.send({ ...ex })
     });
 });
 
-router.delete('/:table/:id', function (req, res, next) {
+router.delete("/:table/:id", (req, res) => {
     /**
      * @api {DELETE} /:table/:id Delete Entity
      * @apiGroup Entities
@@ -242,12 +242,12 @@ router.delete('/:table/:id', function (req, res, next) {
 
     api.Delete({
         ...req.params
-    }).then(function (response) {
+    }).then((response) => {
         res.send({
             ...response,
             newToken: generateToken({ userId: req.userId })
         })
-    }).catch(function (ex) {
+    }).catch((ex) => {
         res.send({ ...ex })
     })
 })
