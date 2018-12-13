baf57ed2a71e33810d505173c94202802be59fca
Ralph Largo <devlargs@gmail.com>
2018-12-13 20:50:26 +0800

    remove duplicate parameter

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((_, res, next) => {
 app.use("/", index);
 app.use("/api", api);
 
-app.use((_, _, next) => {
+app.use((_, res, next) => {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
