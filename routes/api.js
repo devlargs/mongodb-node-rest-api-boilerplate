e33ff474474a2fa8258f88b68d37526758f16fc1
Ralph Largo <devlargs@gmail.com>
2018-12-13 19:55:56 +0800

    Removed inline comments

--- routes/api.js
+++ routes/api.js
@@ -18,16 +18,6 @@ const generateToken = (payload) => {
 }
 
 router.post("/authenticate", (req, res) => {
-    /**
-     * @api {POST} /authenticate Get Token
-     * @apiGroup Authentication
-     *
-     * @apiParam {String} email Users email address
-     * @apiParam {String} password Users password
-     *
-     * @apiSuccess {String} token Used for Authorization header on request
-    */
-
     const { email, password } = req.body;
 
     if (!email) {
@@ -114,23 +104,6 @@ router.get("/:table", async (req, res) => {
 });
 
 router.get("/:table/:id", async (req, res) => {
-    /**
-     * @api {GET} /:table/:id Get Entity
-     * @apiGroup Entities
-     *
-     * @apiParam (Query) {String} table Specify database table/collection
-     * @apiParam (Query) {String} [id] Pass it on /getEntity/:table/:id if you want to return specific data
-     *
-     * @apiParam (Body) {Object} [filter] Pass specific attributes to return filtered queries. e.g. { address: "Makati"}
-     * @apiParam (Body) {Object} [fields] Pass this to return specific attributes that you only need. e.g. ["Name", "Address"] returns [{Name: "", Address: ""}, ...{ and_so_on }] only
-     * 
-     * @apiHeader {String} Authorization Insert generated token here to validate request.
-     * 
-     * @apiSuccess {Number} length Length of the response
-     * @apiSuccess {Object} lists Response content
-     * 
-    */
-
    const collections = await new Promise((resolve) => {
         getCollections((res) => {
             resolve(res);
@@ -163,20 +136,6 @@ router.get("/:table/:id", async (req, res) => {
 });
 
 router.post("/:table", (req, res) => {
-    /**
-     * @api {POST} /:table Post Entity
-     * @apiGroup Entities
-     *
-     * @apiParam (Query) {String} table Specify database table/collection
-     *
-     * @apiParam (Body) {Object} formData All things to be added
-     * 
-     * @apiHeader {String} Authorization Insert generated token here to validate request.
-     * 
-     * @apiSuccess {Number} length Length of the response
-     * @apiSuccess {Object} lists Response content
-     * 
-    */
     api.Post({
         table: req.params.table,
         formData: req.body
@@ -195,22 +154,6 @@ router.put("/:table", (_, res) => {
 });
 
 router.put("/:table/:id", (req, res) => {
-    /**
-     * @api {PUT} /:table/:id Put Entity
-     * @apiGroup Entities
-     *
-     * @apiParam (Query) {String} table Specify database table/collection
-     * @apiParam (Query) {String} id Specify id to be edited
-     *
-     * @apiParam (Body) {Object} formData Payload object that determines all fields to be edited
-     * 
-     * @apiHeader {String} Authorization Insert generated token here to validate request.
-     * 
-     * @apiSuccess {Number} length Length of the response
-     * @apiSuccess {Object} lists Response content
-     * 
-    */
-
     api.Put({
         ...req.params,
         newData: req.body.formData
@@ -227,19 +170,6 @@ router.put("/:table/:id", (req, res) => {
 router.delete("/:table", (_, res) => res.send({ status: 400, message: "Please enter id to delete" }));
 
 router.delete("/:table/:id", (req, res) => {
-    /**
-     * @api {DELETE} /:table/:id Delete Entity
-     * @apiGroup Entities
-     *
-     * @apiParam (Query) {String} table Specify database table/collection
-     * @apiParam (Query) {String} id Specify id to be deleted
-     *
-     * @apiHeader {String} Authorization Insert generated token here to validate request.
-     * 
-     * @apiSuccess {String} id Deleted object id
-     * 
-    */
-
     api.Delete({
         ...req.params
     }).then((response) => {
